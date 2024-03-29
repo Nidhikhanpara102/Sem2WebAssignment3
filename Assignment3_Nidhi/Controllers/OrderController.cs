0b8be301db9455ae17c12285981f247b14f4dade
Nidhikhanpara102 <Nidhikhanpara102@gmail.com>
2024-03-29 13:25:53 -0400

    CRUD Operations for the OrderController has been done

--- Assignment3_Nidhi/Controllers/OrderController.cs
+++ Assignment3_Nidhi/Controllers/OrderController.cs
@@ -17,5 +17,66 @@ namespace Assignment3_Nidhi.Controllers
             _context = context;
         }
 
+        // GET: api/Order
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrder(int id)
+        {
+            var order = _context.Orders.Find(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // POST: api/Order
+        [HttpPost]
+        public ActionResult<Order> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.OrderId }, order);
+        }
+
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public IActionResult PutOrder(int id, Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Order/5
+        [HttpDelete("{id}")]
+        public ActionResult<Order> DeleteOrder(int id)
+        {
+            var order = _context.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+
+            return order;
+        }
     }
 }
