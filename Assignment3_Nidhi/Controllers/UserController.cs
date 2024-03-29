d8397c642da9511081c1dc217ef3909c2172a490
Nidhikhanpara102 <Nidhikhanpara102@gmail.com>
2024-03-29 13:22:37 -0400

    CRUD Operations for the UserController has been done

--- Assignment3_Nidhi/Controllers/UserController.cs
+++ Assignment3_Nidhi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Assignment3_Nidhi.Entities;
 using System.Collections.Generic;
@@ -17,5 +18,67 @@ namespace Assignment3_Nidhi.Controllers
             _context = context;
         }
 
+        // GET: api/User
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound("User does not exist");
+            }
+
+            return user;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public ActionResult<User> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public IActionResult PutUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public ActionResult<User> DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound("User does not exist");
+            }
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return user;
+        }
     }
 }
+
