f97e5354cb90a8c96965915575fa06aaeefda789
Nidhikhanpara102 <Nidhikhanpara102@gmail.com>
2024-03-29 14:04:07 -0400

    CRUD Operations for the CommentController has been performed. Also, performed basic code validation for the Cart Items.

--- Assignment3_Nidhi/Controllers/CommentsController.cs
+++ Assignment3_Nidhi/Controllers/CommentsController.cs
@@ -18,8 +18,72 @@ namespace Assignment3_Nidhi.Controllers
         }
 
 
+        // GET: api/Comments
+        [HttpGet]
+        public ActionResult<IEnumerable<Comments>> GetComments()
+        {
+            return _context.Comments.ToList();
+        }
+
+        // GET: api/Comments/5
+        [HttpGet("{id}")]
+        public ActionResult<Comments> GetComments(int id)
+        {
+            var comments = _context.Comments.Find(id);
+
+            if (comments == null)
+            {
+                return NotFound("Comment Id not found");
+            }
+
+            return comments;
+        }
+
+        // POST: api/Comments
+        [HttpPost]
+        public ActionResult<Comments> PostComments(Comments comments)
+        {
+            var product = _context.Products.Find(comments.ProductId);
+            if (product == null)
+            {
+                return BadRequest("Invalid ProductId");
+            }
+
+            _context.Comments.Add(comments);
+            _context.SaveChanges();
+
+            var successMessage = "Comment added successfully";
+
+            return CreatedAtAction(nameof(GetComments), new { id = comments.CommentId }, new { comments, message = successMessage });
+        }
 
 
+        // PUT: api/Comments/5
+        [HttpPut("{id}")]
+        public IActionResult PutComments(int id, Comments comments)
+        {
+            if (id != comments.CommentId)
+            {
+                return BadRequest("Invalid Comment ID");
+            }
+
+            var existingComment = _context.Comments.Find(id);
+            if (existingComment == null)
+            {
+                return NotFound("Comment not found");
+            }
+
+            existingComment.ProductId = comments.ProductId;
+            existingComment.UserId = comments.UserId;
+            existingComment.Rating = comments.Rating;
+            existingComment.Image = comments.Image;
+            existingComment.Text = comments.Text;
+
+            _context.Entry(existingComment).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok("Comment updated successfully");
+        }
 
     }
 }
