118996e758f4acc2de8410f9a7b53c9de0154724
Nidhikhanpara102 <Nidhikhanpara102@gmail.com>
2024-03-29 13:19:56 -0400

    CRUD Operations for the ProductController has been done

--- Assignment3_Nidhi/Controllers/ProductController.cs
+++ Assignment3_Nidhi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Assignment3_Nidhi.Entities;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,67 @@ namespace Assignment3_Nidhi.Controllers
             _context = context;
         }
 
+        // POST: api/Product
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+
+        // PUT (UPDATE): api/Product/5
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public ActionResult<Product> DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return product;
+        }
     }
 }
-
-
-
